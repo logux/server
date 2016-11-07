0b25f13800c00647e7717a36e16c715744b000c9
Andrey Sitnik <andrey@sitnik.ru>
2016-11-08 00:53:48 +0300

    Fix tests on node.js 4

--- test/servers/throw.js
+++ test/servers/throw.js
@@ -9,5 +9,10 @@ new Server({
 })
 
 setTimeout(function () {
-  throw new Error('Test Error')
+  var error = new Error('Test Error')
+  error.stack = error.stack.split('\n').slice(0, 2).join('\n') + '\n' +
+    '    at ontimeout (timers.js:365:14)\n' +
+    '    at tryOnTimeout (timers.js:237:5)\n' +
+    '    at Timer.listOnTimeout (timers.js:207:5)\n'
+  throw error
 }, 10)
