bc6135d91f4c9e18e3e44986515e45e8e4afdcec
Andrey Sitnik <andrey@sitnik.ru>
2016-11-08 00:33:00 +0300

    Show server test name in updater preview

--- test/servers/update.js
+++ test/servers/update.js
@@ -10,6 +10,7 @@ Object.keys(servers).forEach(function (test) {
   servers[test]().then(function (results) {
     var out = results[0].replace(DATE, '1970-01-01 00:00:00')
     fs.writeFileSync(path.join(__dirname, test + '.out'), out)
+    process.stdout.write('\n' + test + ':\n')
     process.stdout.write(out)
   })
 })
