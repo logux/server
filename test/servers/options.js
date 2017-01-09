c7cecf5c80b53f9bebedd1a858d4e780d6b8532c
Andrey Sitnik <andrey@sitnik.ru>
2017-01-10 04:46:33 +1000

    Clean up options test

--- test/servers/options.js
+++ test/servers/options.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-var fs = require('fs')
-var path = require('path')
 var Server = require('../../server')
 
 var app = new Server({
@@ -10,20 +8,8 @@ var app = new Server({
   supports: '1.x'
 })
 
-var defaults = {
-  port: '1337',
-  host: '127.0.0.1',
-  key: fs.readFileSync(path.join(__dirname, '../fixtures/key.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '../fixtures/cert.pem'))
-}
-
 app.auth(function () {
   return Promise.resolve(true)
 })
 
-var options = app.loadOptions(process, defaults)
-app.listen(options)
-
-setTimeout(function () {
-  app.destroy()
-}, 10)
+app.listen(app.loadOptions(process))
