f249fa76fadf967251d66e97a07e999edc61fcdf
Anton Artamonov <a.artmnv@gmail.com>
2017-03-16 20:09:32 +0700

    Reporter small refactoring

--- reporters/reporter.js
+++ reporters/reporter.js
@@ -5,13 +5,20 @@ const humanErrorReporter = require('./human/error')
 const bunyanProcessReporter = require('./bunyan/process')
 const bunyanErrorReporter = require('./bunyan/error')
 
+function writeBunyanLog (logger, payload) {
+  const details = payload.details || {}
+  logger[payload.level](details, payload.msg)
+}
+
 module.exports = {
   reportProcess (options) {
     const args = Array.prototype.slice.call(arguments, 1)
     if (options.reporter === 'bunyan') {
-      const x = bunyanProcessReporter.apply(null, args)
-      const log = options.bunyanLogger
-      log[x.level](x.details, x.msg)
+      if (!options.bunyanLogger) {
+        throw new Error('Missed bunyan logger')
+      }
+      const payload = bunyanProcessReporter.apply(null, args)
+      writeBunyanLog(options.bunyanLogger, payload)
     } else {
       process.stderr.write(humanProcessReporter.apply(null, args))
     }
@@ -19,9 +26,11 @@ module.exports = {
 
   reportRuntimeError (options, e) {
     if (options.reporter === 'bunyan') {
-      const x = bunyanErrorReporter(e, this)
-      const log = options.bunyanLogger
-      log[x.level](x.details, x.msg)
+      if (!options.bunyanLogger) {
+        throw new Error('Missed bunyan logger')
+      }
+      const payload = bunyanErrorReporter(e, this)
+      writeBunyanLog(options.bunyanLogger, payload)
     } else {
       process.stderr.write(humanErrorReporter(e, this))
     }
