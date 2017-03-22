e20fe1d36b6eaa361bad9934cdab45d507ec2d31
Andrey Sitnik <andrey@sitnik.ru>
2017-03-22 16:38:02 +0300

    More safer uncatch test

--- test/servers/uncatch.js
+++ test/servers/uncatch.js
@@ -17,5 +17,5 @@ new Promise((resolve, reject) => {
     const error = new Error('Test Error')
     error.stack = `${ error.stack.split('\n')[0] }\nfake stacktrace`
     reject(error)
-  }, 10)
+  }, 50)
 })
