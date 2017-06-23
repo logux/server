7ffb58ec512d5d9d8e75cf1a74880c01e19aa8a6
Andrey Sitnik <andrey@sitnik.ru>
2017-06-23 00:18:29 -0300

    Add test for Server#undo

--- test/base-server.test.js
+++ test/base-server.test.js
@@ -538,3 +538,29 @@ it('reports about error during action processing', () => {
     })
   })
 })
+
+it('undos actions on client', () => {
+  app = createServer()
+  app.undo({ id: [1, '10:uuid', 0] }, 'magic')
+  return wait(1).then(() => {
+    const entries = app.log.store.created.map(i => i.slice(0, 2))
+    expect(entries).toEqual([
+      [
+        {
+          type: 'logux/undo',
+          id: [1, '10:uuid', 0],
+          reason: 'magic'
+        },
+        {
+          id: [1, 'server:uuid', 0],
+          time: 1,
+          added: 1,
+          nodeIds: ['10:uuid'],
+          reasons: ['error'],
+          server: 'server:uuid',
+          status: 'processed'
+        }
+      ]
+    ])
+  })
+})
