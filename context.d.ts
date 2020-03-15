a5b7cd4eb3add71052d4def8e3d4414587c0abc2
Kirill Neruchev <kirill@neruchev.com>
2020-03-15 11:37:11 +0100

    Add Context types

---
+++ context.d.ts
@@ -0,0 +1,160 @@
+import { Action, Meta, BaseServer } from './base-server'
+
+/**
+ * Action context.
+ *
+ * ```js
+ * server.type('FOO', {
+ *   access (ctx, action, meta) {
+ *     return ctx.isSubprotocol('3.x') ? check3(action) : check4(action)
+ *   }
+ * })
+ * ```
+ */
+export class Context {
+  /**
+   * @param nodeId Unique node ID.
+   * @param clientId Unique persistence client ID.
+   * @param userId User ID taken node ID.
+   * @param subprotocol Action creator application subprotocol version in SemVer format.
+   * @param server Logux server
+   */
+  constructor(
+    nodeId: string,
+    clientId: string,
+    userId: string | undefined,
+    subprotocol: LoguxServerBaseOptions['subprotocol'],
+    server: BaseServer
+  )
+
+  /**
+   * Open structure to save some data between different steps of processing.
+   *
+   * ```js
+   * server.type('RENAME', {
+   *   access (ctx, action, meta) {
+   *     ctx.data.user = findUser(ctx.userId)
+   *     return ctx.data.user.hasAccess(action.projectId)
+   *   }
+   *   process (ctx, action, meta) {
+   *     return ctx.data.user.rename(action.projectId, action.name)
+   *   }
+   * })
+   * ```
+   */
+  data: Object
+
+  /**
+   * Unique node ID.
+   *
+   * ```js
+   * server.nodeIds[node.nodeId]
+   * ```
+   */
+  nodeId: string
+
+  /**
+   * User ID taken node ID.
+   *
+   * ```js
+   * async access (ctx, action, meta) {
+   *   const user = await db.getUser(ctx.userId)
+   *   return user.admin
+   * }
+   * ```
+   */
+  userId: 'server' | string | undefined
+
+  /**
+   * Unique persistence client ID.
+   *
+   * ```js
+   * server.clientIds[node.clientId]
+   * ```
+   */
+  clientId: string
+
+  /**
+   * Was action created by Logux server.
+   *
+   * ```js
+   * access: (ctx, action, meta) => ctx.isServer
+   * ```
+   */
+  isServer: boolean
+
+  /**
+   * Action creator application subprotocol version in SemVer format.
+   * Use {@link Context#isSubprotocol} to check it.
+   */
+  subprotocol: string | undefined
+
+  /**
+   * Logux server
+   */
+  server: BaseServer
+
+  /**
+   * Check creator subprotocol version. It uses `semver` npm package
+   * to parse requirements.
+   *
+   * @param range npm’s version requirements.
+   * @returns Is version satisfies requirements.
+   *
+   * ```js
+   * if (ctx.isSubprotocol('2.x')) {
+   *   useOldAPI()
+   * }
+   * ```
+   */
+  isSubprotocol: (range: string) => boolean
+
+  /**
+   * Send action back to the client.
+   *
+   * @param action The action.
+   * @param meta Action’s meta.
+   *
+   * @returns Promise until action was added to the server log.
+   *
+   * ```js
+   * ctx.sendBack({ type: 'login/success', token })
+   * ```
+   */
+  public sendBack: (action: Action, meta: Meta) => Promise<void>
+}
+
+/**
+ * Subscription context.
+ *
+ * ```js
+ * server.channel('user/:id', {
+ *   access (ctx, action, meta) {
+ *     return ctx.params.id === ctx.userId
+ *   }
+ * })
+ * ```
+ */
+export class ChannelContext<
+  PatternParams extends Object | Array<string> = {}
+> extends Context {
+  /**
+   * Parsed variable parts of channel pattern.
+   *
+   * ```js
+   * server.channel('user/:id', {
+   *   access (ctx, action, meta) {
+   *     action.channel //=> user/10
+   *     ctx.params //=> { id: '10' }
+   *   }
+   * })
+   * server.channel(/post\/(\d+)/, {
+   *   access (ctx, action, meta) {
+   *     action.channel //=> post/10
+   *     ctx.params //=> ['post/10', '10']
+   *   }
+   * })
+   * ```
+   */
+  public params?: PatternParams
+}
--- context.js
+++ context.js
@@ -1,148 +1,23 @@
 let semver = require('semver')
 
-/**
- * Action context.
- *
- * @example
- * server.type('FOO', {
- *   access (ctx, action, meta) {
- *     return ctx.isSubprotocol('3.x') ? check3(action) : check4(action)
- *   }
- * })
- */
 class Context {
   constructor (nodeId, clientId, userId, subprotocol, server) {
-    /**
-     * Open structure to save some data between different steps of processing.
-     * @type {object}
-     *
-     * @example
-     * server.type('RENAME', {
-     *   access (ctx, action, meta) {
-     *     ctx.data.user = findUser(ctx.userId)
-     *     return ctx.data.user.hasAccess(action.projectId)
-     *   }
-     *   process (ctx, action, meta) {
-     *     return ctx.data.user.rename(action.projectId, action.name)
-     *   }
-     * })
-     */
     this.data = { }
-    /**
-     * Unique node ID.
-     * @type {string}
-     *
-     * @example
-     * server.nodeIds[node.nodeId]
-     */
     this.nodeId = nodeId
-
-    /**
-     * User ID taken node ID.
-     * @type {string|undefined}
-     *
-     * @example
-     * async access (ctx, action, meta) {
-     *   const user = await db.getUser(ctx.userId)
-     *   return user.admin
-     * }
-     */
     this.userId = userId
-    /**
-     * Unique persistence client ID.
-     * @type {string}
-     *
-     * @example
-     * server.clientIds[node.clientId]
-     */
     this.clientId = clientId
-
-    /**
-     * Was action created by Logux server.
-     * @type {boolean}
-     *
-     * @example
-     * access: (ctx, action, meta) => ctx.isServer
-     */
     this.isServer = userId === 'server'
-
-    /**
-     * Action creator application subprotocol version in SemVer format.
-     * Use {@link Context#isSubprotocol} to check it.
-     * @type {string|undefined}
-     */
     this.subprotocol = subprotocol
-
     this.server = server
   }
 
-  /**
-   * Check creator subprotocol version. It uses `semver` npm package
-   * to parse requirements.
-   *
-   * @param {string} range npm’s version requirements.
-   *
-   * @return {boolean} Is version satisfies requirements.
-   *
-   * @example
-   * if (ctx.isSubprotocol('2.x')) {
-   *   useOldAPI()
-   * }
-   */
   isSubprotocol (range) {
     return semver.satisfies(this.subprotocol, range)
   }
 
-  /**
-   * Send action back to the client.
-   *
-   * @param {Action} action The action.
-   * @param {Meta} [meta]   Action’s meta.
-   *
-   * @return {Promise} Promise until action was added to the server log.
-   *
-   * @example
-   * ctx.sendBack({ type: 'login/success', token })
-   */
   sendBack (action, meta = { }) {
     return this.server.log.add(action, { clients: [this.clientId], ...meta })
   }
 }
 
 module.exports = Context
-
-/**
- * Subscription context.
- *
- * @name ChannelContext
- * @class
- * @extends Context
- *
- * @example
- * server.channel('user/:id', {
- *   access (ctx, action, meta) {
- *     return ctx.params.id === ctx.userId
- *   }
- * })
- */
-/**
- * Parsed variable parts of channel name.
- *
- * @name params
- * @type {object}
- * @memberof ChannelContext#
- *
- * @example
- * server.channel('user/:id', {
- *   access (ctx, action, meta) {
- *     action.channel //=> user/10
- *     ctx.params //=> { id: '10' }
- *   }
- * })
- * server.channel(/post\/(\d+)/, {
- *   access (ctx, action, meta) {
- *     action.channel //=> post/10
- *     ctx.params //=> ['post/10', '10']
- *   }
- * })
- */
