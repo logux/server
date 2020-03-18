92a86225310212c22e8bde496bb9aac893287746
Andrey Sitnik <andrey@sitnik.ru>
2020-03-18 01:57:46 -0400

    Add ESM support

---
+++ index.mjs
@@ -0,0 +1,5 @@
+import ALLOWED_META from './allowed-meta/index.js'
+import BaseServer from './base-server/index.js'
+import Server from './server/index.js'
+
+export { BaseServer, Server, ALLOWED_META }
