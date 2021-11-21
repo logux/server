5fffb7ee4fd8f066c0f4e80eb39d70ea15d76dbf
Andrey Sitnik <andrey@sitnik.ru>
2021-11-21 21:26:32 +0100

    Fix Node.js 12 support

--- test/resolver.cjs
+++ test/resolver.cjs
@@ -1,16 +1,18 @@
-// temporary workaround while we wait for https://github.com/facebook/jest/issues/9771
-const importResolver = require('enhanced-resolve').create.sync({
+let enhanced = require('enhanced-resolve')
+
+let importResolver = enhanced.create.sync({
   conditionNames: ['import', 'node', 'default'],
   extensions: ['.js', '.json', '.node', '.ts']
 })
-const requireResolver = require('enhanced-resolve').create.sync({
+let requireResolver = enhanced.create.sync({
   conditionNames: ['require', 'node', 'default'],
   extensions: ['.js', '.json', '.node', '.ts']
 })
 
+// temporary workaround while we wait for https://github.com/facebook/jest/issues/9771
 module.exports = function (request, options) {
   let resolver = requireResolver
-  if (options.conditions?.includes('import')) {
+  if (options.conditions && options.conditions.includes('import')) {
     resolver = importResolver
   }
   return resolver(options.basedir, request)
