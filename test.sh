d1f9619507132ec1948af235a704d0b774dc1304
Bijela Gora <bijela_gora@protonmail.com>
2022-04-03 22:46:05 +0200

    Remove scaffold

--- test.sh
+++
@@ -1,8 +0,0 @@
-podman build -q -t ubuntu-test -f ./ubuntu.dockerfile . && \
-podman run --rm \
-  --cpus=2 \
-  --memory=7g \
-  --publish 9229:9229 \
-  --publish 9339:9339 \
-  localhost/ubuntu-test:latest \
-  node --experimental-vm-modules node_modules/jest/bin/jest.js --ci --detectOpenHandles
