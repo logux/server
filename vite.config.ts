3344fb1807b2b097dbc8fc9962113a5f676589d1
Andrey Sitnik <andrey@sitnik.ru>
2023-12-06 07:06:51 +0100

    Update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,19 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     coverage: {
-      exclude: ['node_modules', 'server/index.js', '*/*.test.*'],
-      lines: 100,
-      provider: 'v8'
+      exclude: [
+        'node_modules',
+        'server/index.js',
+        'test/*',
+        '**/*.d.ts',
+        '*/errors.ts',
+        '*/types.ts',
+        'human-formatter'
+      ],
+      provider: 'v8',
+      thresholds: {
+        lines: 100
+      }
     },
     environment: 'node',
     exclude: ['node_modules', 'test/servers']
